e5b2582934fe8a6b2218a501f02e3c9636a5eca0
Kim-one <144210806+Kim-one@users.noreply.github.com>
2025-03-29 18:00:20 -0300

    added backdrop

--- frontend/app/src/pages/Home.tsx
+++ frontend/app/src/pages/Home.tsx
@@ -1,18 +1,34 @@
 import "./Home.css";
+import bubbleguy from "../assets/We’re ranting to a resolution.png";
+// import backdrop from "../assets/backdrop.png";
+// import speaker from "../assets/peaker.png";
+
 function Home() {
   return (
     <div>
+      {/* <img className="backdrop" src={backdrop} alt="" /> */}
       <div className="Header">
         <div>
-          <h1>Welcome to Halijams</h1>
+          <h1>Lets rant about it!</h1>
         </div>
       </div>
-      <div className="rants">
-        <p>Rants to Resolution</p>
+      <div className="bubble">
+        <span>
+          <img className="chatbubble" src={bubbleguy} alt="chat bubble"></img>
+          <p>Ranting to a resolution!</p>
+        </span>
       </div>
-      <div>
-        <a href="/map">Rants</a>
+      <div className="rants-btn">
+        <a href="/map">Get Started</a>
       </div>
+      <div className="home"></div>
+      {/* <div className="speaker">
+        <img
+          className="background_speaker"
+          src={speaker}
+          alt="speakerphone"
+        ></img>
+      </div> */}
     </div>
   );
 }
