2277b735455440de9a47c397e8c23526d22166a5
newcombe <cn728016@dal.ca>
2025-03-29 11:49:30 -0300

    Importing SharedModule

--- frontend/halirant-app/src/app/core/components/navbar/navbar.component.ts
+++ frontend/halirant-app/src/app/core/components/navbar/navbar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
