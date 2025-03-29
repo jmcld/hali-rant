2b5ed6ac96a20eb806a2301a051cb48bc4bcf30d
Harvey Wang <harvey@dal.ca>
2025-03-29 12:13:14 -0300

    Move about page

--- frontend/halirant-app/src/app/app.routes.ts
+++ frontend/halirant-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo:'home'},
+  { path: 'about', component: AboutComponent },
   { path: '**', component: HomeComponent }, // 404
 ];
