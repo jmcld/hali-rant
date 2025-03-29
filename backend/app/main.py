56b2b4e0cfcd5203fbc79c568c9c692aa2acf996
Kyle Bostelmann <kyle@bostelk.ca>
2025-03-29 15:02:31 -0300

    Add CORS middleware.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .models import models
 from .db import insert_rant
 #from .llm import
@@ -12,6 +14,19 @@ import gel
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
