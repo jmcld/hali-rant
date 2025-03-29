ca1982f2148f47df7c1e5f8ec5980c1939603f15
Sam Silver <sam55silver@gmail.com>
2025-03-29 13:39:16 -0300

    add in opening

--- frontend/app/src/pages/MapPage.tsx
+++ frontend/app/src/pages/MapPage.tsx
@@ -1,9 +1,22 @@
-import Map from "../component/MapWrapper";
+import { useState } from "react";
+import Map from "../components/MapWrapper";
+import MessageSubmit from "../components/MessageSubmit";
 
 const MapPage = () => {
+  const [selectedLocation, setSelectedLocation] = useState<
+    [number, number] | null
+  >(null);
+  const [showModal, setShowModal] = useState(false);
+
+  const handleMapClick = (latlng: any) => {
+    setSelectedLocation(latlng);
+    setShowModal(true);
+  };
+
   return (
     <div>
-      <Map ClickHandler={() => {}} />
+      <Map ClickHandler={handleMapClick} />
+      <MessageSubmit isOpen={showModal} onClose={() => setShowModal(false)} />
     </div>
   );
 };
