d4a396c5fea46d9b07e286cc987e8213c856ea2d
Kim-one <144210806+Kim-one@users.noreply.github.com>
2025-03-29 13:27:42 -0300

    Link to map

--- frontend/app/src/pages/Home.tsx
+++ frontend/app/src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import "./Home.css";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="Header">
+        <div>
+          <h1>Welcome to Halijams</h1>
+        </div>
+      </div>
+      <div className="rants">
+        <p>Rants to Resolution</p>
+      </div>
+      <div>
+        <a href="/map">Rants</a>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
