09c7d4e1ddbf3b317bd7cf1438e0783a8a3d3d18
Sam Silver <sam55silver@gmail.com>
2025-03-29 13:34:32 -0300

    change map up

--- frontend/app/src/pages/MapPage.tsx
+++ frontend/app/src/pages/MapPage.tsx
@@ -1,22 +1,9 @@
-import { useState } from "react";
-import Map from "../components/MapWrapper";
-import MessageSubmit from "../components/MessageSubmit";
+import Map from "../component/MapWrapper";
 
 const MapPage = () => {
-  const [selectedLocation, setSelectedLocation] = useState<
-    [number, number] | null
-  >(null);
-  const [showModal, setShowModal] = useState(false);
-
-  const handleMapClick = (latlng: any) => {
-    setSelectedLocation(latlng);
-    setShowModal(true);
-  };
-
   return (
     <div>
-      <Map ClickHandler={handleMapClick} />
-      <MessageSubmit isOpen={showModal} onClose={() => setShowModal(false)} />
+      <Map ClickHandler={() => {}} />
     </div>
   );
 };
