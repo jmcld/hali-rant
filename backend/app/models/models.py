8d29b5d5e3d14d60a09cdc04e3fdf4203cf0ef7b
Kyle Bostelmann <kyle@bostelk.ca>
2025-03-29 12:06:58 -0300

    Update data model
    Canabalzie the message model into votable, time and location.

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -4,27 +4,29 @@ from typing import List
 
 from pydantic import BaseModel
 
+
+class TimeModel(BaseModel):
+    created_at: datetime
+    updated_at: datetime
+
+
 class LocationModel(BaseModel):
     lat: float
     lon: float
 
-class MessageModel(BaseModel):
-    title: str
-    descr: str
+
+class VotableModel(BaseModel):
     nLike: int
     nDislike: int
-    loc: LocationModel
-    timestamp: datetime
-    id: uuid.UUID
-    parent: int
-    category: str
-    # Future considerations:
-    # attachements: uuid.UUID
+
 
 class ReplyModel(BaseModel):
     id: uuid.UUID
     rantId: uuid.UUID
     msg: str
+    votes: VotableModel
+    time: TimeModel
+
 
 class RantModel(BaseModel):
     id: uuid.UUID
@@ -32,27 +34,52 @@ class RantModel(BaseModel):
     title: str
     body: str
     categ: str
+    votes: VotableModel
+    location: LocationModel
+    time: TimeModel
 
 
 if __name__ == "__main__":
+    now = TimeModel(
+        created_at=datetime.now(),
+        updated_at=datetime.now(),
+    )
+
+    empty_vote = VotableModel(
+        nLike=0,
+        nDislike=0
+    )
+
     reply0 = ReplyModel(
-        id = uuid.uuid4(),
-        rantId = uuid.uuid4(),
-        msg = "I know mine too!"
+        id=uuid.uuid4(),
+        rantId=uuid.uuid4(),
+        msg="I know mine too!",
+        votes=empty_vote,
+        time=now
     )
 
     reply1 = ReplyModel(
         id=uuid.uuid4(),
         rantId=uuid.uuid4(),
-        msg="You should watch the road when you drive."
+        msg="You should watch the road when you drive.",
+        votes=empty_vote,
+        time=now
+    )
+
+    halifax = LocationModel(
+        lat=44,
+        lon=-63
     )
 
     rant = RantModel(
-        id = uuid.uuid4(),
-        title = "Potholes",
-        body = "This pothole destroyed my car",
-        categ = "😭",
-        reply = [reply0, reply1]
+        id=uuid.uuid4(),
+        title="Potholes",
+        body="This pothole destroyed my car",
+        categ="😭",
+        reply=[reply0, reply1],
+        location=halifax,
+        votes=empty_vote,
+        time=now
     )
 
     print(rant.model_dump_json(indent=2))
